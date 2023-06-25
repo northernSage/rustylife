c0f18a576257c19871370b10ae3989af36111bc9
northernSage <gfvante@gmail.com>
2023-06-24 23:26:21 -0300

    add cell state enum

--- src/main.rs
+++ src/main.rs
@@ -14,62 +14,59 @@ use pixel_canvas::{
     input::glutin::event::{ElementState, VirtualKeyCode},
     Canvas, Color,
 };
-use rand::{self, Rng};
+use rand::{self, Rng, prelude::Distribution, distributions::Standard};
 
 mod keyboard;
 pub use crate::keyboard::KeyboardState;
+
+#[derive(Clone, PartialEq, Copy)]
+enum State {
+    Alive,
+    Dead
+}
 
 struct Cells {
     // table with the state of every single cell:
     // alive = true
     // dead = false
-    states: [[bool; 512]; 512],
+    states: [[State; 512]; 512],
 }
 
 impl Cells {
     fn new() -> Self {
         Self {
-            states: [[false; 512]; 512],
+            states: [[State::Dead; 512]; 512],
         }
     }
 
-    fn glider(&mut self, x: usize, y: usize) {
-        // ###
-        // #
-        //  #
-        self.states[y][x] = true;
-        self.states[y][x - 1] = true;
-        self.states[y][x - 2] = true;
-        self.states[y - 1][x - 2] = true;
-        self.states[y - 2][x - 1] = true;
-    }
-
     fn mosaic(&mut self) {
-        self.states[255][255] = true;
-        self.states[255][256] = true;
-        self.states[255][257] = true;
+        self.states[255][255] = State::Alive;
+        self.states[255][256] = State::Alive;
+        self.states[255][257] = State::Alive;
         //
-        self.states[10][10] = true;
-        self.states[10][11] = true;
-        self.states[10][12] = true;
+        self.states[10][10] = State::Alive;
+        self.states[10][11] = State::Alive;
+        self.states[10][12] = State::Alive;
         //
-        self.states[10][500] = true;
-        self.states[10][501] = true;
-        self.states[10][502] = true;
+        self.states[10][500] = State::Alive;
+        self.states[10][501] = State::Alive;
+        self.states[10][502] = State::Alive;
         //
-        self.states[500][10] = true;
-        self.states[500][11] = true;
-        self.states[500][12] = true;
+        self.states[500][10] = State::Alive;
+        self.states[500][11] = State::Alive;
+        self.states[500][12] = State::Alive;
         //
-        self.states[500][500] = true;
-        self.states[500][501] = true;
-        self.states[500][502] = true;
+        self.states[500][500] = State::Alive;
+        self.states[500][501] = State::Alive;
+        self.states[500][502] = State::Alive;
     }
 
     fn randomize(&mut self) {
         for y in 1..511 {
             for x in 1..511 {
-                self.states[y][x] = rand::random();
+                if rand::random() {
+                    self.states[y][x] = State::Alive;
+                }
             }
         }
     }
@@ -90,7 +87,7 @@ impl Cells {
 
 struct Life {
     cells: Cells,
-    states_lookup: [[bool; 512]; 512],
+    states_lookup: [[State; 512]; 512],
 }
 
 impl Life {
@@ -110,18 +107,18 @@ impl Life {
         // count living neighbors
         let mut living_neighbors = 0;
         for (ny, nx) in self.cells.neighbor_indexes(x, y) {
-            if self.states_lookup[ny][nx] {
+            if self.states_lookup[ny][nx] == State::Alive {
                 living_neighbors += 1;
             }
         }
 
         // alive
-        if self.states_lookup[y][x] {
+        if self.states_lookup[y][x] == State::Alive {
             // stays alive if it has either 2 or 3 live neighbors
             if living_neighbors != 2 && living_neighbors != 3 {
                 // coocentric expansion in all directions
                 // if living_neighbors < 2 {
-                self.cells.states[y][x] = false;
+                self.cells.states[y][x] = State::Dead;
             }
         // dead
         } else {
@@ -129,7 +126,7 @@ impl Life {
             if living_neighbors == 3 {
                 // coocentric expansion in all directions
                 // if living_neighbors == 2 {
-                self.cells.states[y][x] = true;
+                self.cells.states[y][x] = State::Alive;
             }
         }
     }
@@ -147,7 +144,11 @@ impl Life {
 
         for (y, l) in pattern.iter().enumerate() {
             for (x, c) in l.iter().enumerate() {
-                self.cells.states[y + pos_y][x + pos_x] = *c != '.';
+                if *c != '.' {
+                    self.cells.states[y + pos_y][x + pos_x] = State::Alive;
+                } else {
+                    self.cells.states[y + pos_y][x + pos_x] = State::Dead;
+                }
             }
         }
     }
@@ -200,7 +201,7 @@ fn main() {
 
                 life.apply_rules(x, y);
 
-                if life.cells.states[y][x] {
+                if life.cells.states[y][x] == State::Alive {
                     // let mut red  = ((*pixel).r + 1)  % 255;
                     // let mut green  = ((*pixel).g + 1) % 255;
                     // let mut blue = ((*pixel).b + 1) % 255;
